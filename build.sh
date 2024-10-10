e0af7bec56bbf58e222669fcfa59b7b63bc10aa5
shadichy <hotrobb360web@gmail.com>
2024-10-11 00:04:46 +0700

    initial

---
+++ build.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+
+set -e
+
+unset TMP TEMP TMPDIR || true
+
+# might not be exported if we're running from init=/bin/sh or similar
+export d="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+DEBOOTSTRAP_DIR=/usr/share/debootstrap
+dscriptdir=$DEBOOTSTRAP_DIR/scripts/
+
+err() { echo -e "ERROR: $* !" && clean && exit 1; }
+argerr() { err "Cannot set build to empty"; }
+cmderr() { err "Command failed"; }
+cderr() { err "d build is not found"; }
+
+clean() {
+  umount build
+  rm -f $DEBOOTSTRAP_DIR/arch
+
+  rm -f "$dscriptdir/$branch"
+  if [ -f "$dscriptdir/$branch.bak" ]; then
+    mv "$dscriptdir/$branch.bak" "$dscriptdir/$branch"
+  fi
+
+  return 0
+}
+
+arch() {
+  case "$1" in
+  x86_64) echo amd64 ;;
+  i?86) echo i386 ;;
+  aarch64 | arm64) echo arm64 ;;
+  armv*) echo armhf ;;
+  *) echo "$1" ;;
+  esac
+}
+
+# shellcheck disable=SC2068
+if (($(id -u) != 0)); then
+  if [ "$(command -v sudo)" ]; then
+    sudo "$0" $@
+  elif [ "$(command -v doas)" ]; then
+    doas "$0" $@
+  else
+    err "Please run this command as root"
+  fi
+  exit $?
+fi
+
+trap 'err Command returned $?' EXIT
+
+while [[ $1 ]]; do
+  case $1 in
+  -a | --arch) [ -z "$2" ] && argerr build || arch="$2" ;;
+  -m | --mirror) [ -z "$2" ] && argerr build || mirror="$2" ;;
+  -b | --branch) [ -z "$2" ] && argerr build || branch="$2" ;;
+  -o | --dist) [ -z "$2" ] && argerr build || dist="$2" ;;
+  *) break ;;
+  esac
+  shift 2
+done
+
+default_mirror=http://deb.devuan.org/merged
+[ -z "$mirror" ] && read -rp "Enter mirror (default: $default_mirror): " mirror
+mirror=${mirror:-$default_mirror}
+
+default_branch=excalibur
+[ -z "$branch" ] && read -rp "Enter branch (default: $default_branch): " branch
+branch=${branch:-$default_branch}
+
+default_arch=$(arch "$(uname -m)")
+[ -z "$arch" ] && read -rp "Enter architecture: (default: $default_arch): " arch
+arch=${arch:-$default_arch}
+export CARCH=$arch
+
+[ "$dist" ] || dist=./dist/installer.sfs
+echo "Generate to $dist"
+
+PWD=$(pwd)
+
+rm -rf build dist tmp
+mkdir -p build dist tmp
+
+build_chroot() {
+  dd if=/dev/zero of=base.img bs=1M count=1 seek=1024
+  mkfs.ext4 base.img
+
+  mount base.img build -o loop
+
+  if [ -f "$dscriptdir/$branch" ]; then
+    mv "$dscriptdir/$branch" "$dscriptdir/$branch.bak"
+  fi
+  sed "s|@mirror@|$mirror|g" scripts/blissos >"$dscriptdir/$branch"
+
+  echo "$arch" >$DEBOOTSTRAP_DIR/arch
+
+  debootstrap \
+    --arch "$arch" \
+    --variant minbase \
+    --merged-usr \
+    --verbose \
+    --no-check-certificate \
+    --no-check-gpg \
+    "$branch" build
+
+  cp -r template/* build
+}
+
+if [ -f base.img ]; then
+  mount base.img build -o loop
+else
+  build_chroot
+fi
+
+mkdir -p tmp/upper tmp/work
+
+mount overlay build -t overlay -o lowerdir=build,upperdir=tmp/upper,workdir=tmp/work
+mkdir -p tmp/upper tmp/work
+
+mount proc build/proc -t proc -o nosuid,noexec,nodev
+mount sys build/sys -t sysfs -o nosuid,noexec,nodev,ro
+mount udev build/dev -t devtmpfs -o mode=0755,nosuid
+mount devpts build/dev/pts -t devpts -o mode=0620,gid=5,nosuid,noexec
+mount shm build/dev/shm -t tmpfs -o mode=1777,nosuid,nodev
+mount run build/run -t tmpfs -o nosuid,nodev,mode=0755
+mount tmp build/tmp -t tmpfs -o mode=1777,strictatime,nodev,nosuid
+
+grep -Ev '^#' pkglist.txt | xargs chroot build /bin/apt install -y --no-install-recommends --no-install-suggests
+grep -Ev '^#' rmlist.txt | xargs chroot build /bin/dpkg --remove --force-depends
+
+cp -n build/etc/init.d tmp/upper/etc
+
+for d in \
+  bin \
+  sbin \
+  lib \
+  lib32 \
+  lib64; do
+  if [ -d build/usr/$d ]; then
+    ln -s usr/$d tmp/upper
+  fi
+done
+
+for d in \
+  dev \
+  sys \
+  run \
+  proc \
+  tmp \
+  android \
+  boot/grub \
+  boot/efi \
+  cdrom \
+  root; do
+  mkdir -p tmp/upper/$d
+done
+
+for d in \
+  data \
+  data_mirror \
+  system \
+  storage \
+  sdcard \
+  apex \
+  linkerconfig \
+  debug_ramdisk \
+  system_ext \
+  product \
+  vendor; do
+  ln -s android/$d build
+done
+
+echo 'blissos' >build/etc/hostname
+
+cat <<EOF >build/usr/sbin/autologin
+#!/bin/sh
+exec login -f root
+EOF
+chmod +x build/usr/sbin/autologin
+sed -i 's@1::respawn:/sbin/getty@1::respawn:/sbin/getty -n -l /usr/sbin/autologin@g' build/etc/inittab
+sed -i -r 's|^(root:.*:)/bin/d?a?sh$|\1/bin/bash|g' build/etc/passwd
+
+# shellcheck disable=SC2016
+echo '[ -z "$DISPLAY" ] && { startx /usr/bin/xterm; poweroff; }' >build/root/.bash_profile
+chmod +x build/root/.bash_profile
+
+for d in \
+  etc/apt \
+  usr/include \
+  usr/lib/cmake \
+  usr/share/doc \
+  usr/share/doc-base \
+  usr/share/gtk-doc \
+  usr/share/info \
+  usr/share/man \
+  usr/share/man-db \
+  var/cache/* \
+  var/lib/apt \
+  var/lib/dpkg; do
+  rm -rf tmp/upper/"$d"
+done
+
+for mnt in \
+  tmp \
+  run \
+  dev/shm \
+  dev/pts \
+  dev \
+  sys \
+  proc \
+  /; do
+  umount "build/$mnt"
+done
+
+clean
+
+mksquashfs tmp/upper "$dist" -comp zstd -Xcompression-level 22 -b 1M -noappend -no-duplicates -no-recovery -always-use-fragments
